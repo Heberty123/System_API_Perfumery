86e837ee56255a951f006129dfe9ae505847d930
Heberty <hebertyprado22005@gmail.com>
2022-05-31 16:04:34 -0300

    Project Created

---
+++ Amelia/src/main/java/Loja/Amelia/AmeliaApplication.java
@@ -0,0 +1,13 @@
+package Loja.Amelia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmeliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmeliaApplication.class, args);
+	}
+
+}
---
+++ Amelia/src/test/java/Loja/Amelia/AmeliaApplicationTests.java
@@ -0,0 +1,13 @@
+package Loja.Amelia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmeliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
